0f493286723c917e3617a2a8e122ae542f07d580
Павел Цветов <ptsvetov@MacBook-Pro-Admin-33.local>
2017-11-24 10:05:18 +0300

    master. Поле username уникальное

--- src/chat_take_aiohttp/models/users.py
+++ src/chat_take_aiohttp/models/users.py
@@ -3,7 +3,7 @@ from chat_take_aiohttp.models.base_model import BaseModel
 
 
 class Users(BaseModel):
-    username = peewee.CharField(max_length=10, null=False)
+    username = peewee.CharField(max_length=10, null=False, unique=True)
     password = peewee.CharField(max_length=10, null=False)
 
     def __str__(self):
